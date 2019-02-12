735d5347c2bd52d6d81de68601f8124e0458a761
miyamoto <miyamotok0105@gmail.com>
2019-02-12 18:38:25 +0900

    Add swagger sample

--- web-sample/flask-swagger_simple1/app.py
+++ web-sample/flask-swagger_simple1/app.py
@@ -2,9 +2,14 @@
 from flask import render_template
 import connexion
 
-app = connexion.App(__name__, specification_dir='./')
-# app = Flask(__name__, template_folder="templates")
-app.add_api('swagger.yml')
+# app = connexion.App(__name__, specification_dir='./')
+# # app = Flask(__name__, template_folder="templates")
+# app.add_api('swagger.yml')
+
+# app = connexion.App(__name__, specification_dir='./')
+# app.add_api('swagger.yml')
+app = connexion.App(__name__, specification_dir='./models/')
+app.add_api('swagger_models.yml')
 
 @app.route('/')
 def home():
---
+++ web-sample/flask-swagger_simple1/models/user.py
@@ -0,0 +1,54 @@
+# System modules
+from datetime import datetime
+
+# 3rd party modules
+from flask import make_response, abort
+
+
+def get_timestamp():
+    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+
+PEOPLE = {
+    "Farrell": {
+        "fname": "Doug",
+        "lname": "Farrell",
+        "timestamp": get_timestamp(),
+    },
+    "Brockman": {
+        "fname": "Kent",
+        "lname": "Brockman",
+        "timestamp": get_timestamp(),
+    },
+    "Easter": {
+        "fname": "Bunny",
+        "lname": "Easter",
+        "timestamp": get_timestamp(),
+    },
+}
+
+
+def read_all():
+    print("read_all!!")
+    return [PEOPLE[key] for key in sorted(PEOPLE.keys())]
+
+
+
+# def create(person):
+#     print("create!!")
+#     lname = person.get("lname", None)
+#     fname = person.get("fname", None)
+
+#     # Does the person exist already?
+#     if lname not in PEOPLE and lname is not None:
+#         PEOPLE[lname] = {
+#             "lname": lname,
+#             "fname": fname,
+#             "timestamp": get_timestamp(),
+#         }
+#         return PEOPLE[lname], 201
+#     else:
+#         abort(
+#             406,
+#             "Peron with last name {lname} already exists".format(lname=lname),
+#         )
+
