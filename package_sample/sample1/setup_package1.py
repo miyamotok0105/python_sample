e59f0590518392805293dafde76c7366f0c8c830
miyamoto <miyamotok0105@gmail.com>
2019-02-21 18:32:38 +0900

    Add setup script

---
+++ package_sample/sample1/setup_package1.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='package1',
+    version="0.0.1",
+    description="",
+    long_description="",
+    author='',
+    license='MIT',
+    classifiers=[
+        "Development Status :: 1 - Planning"
+    ],
+    keywords=''
+)
